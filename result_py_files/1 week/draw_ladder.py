d52f105ea0b3b169f3bb6cce383a40cb6c90e2f3
Evgenyi Yakimchuk <Kum4yk@gmail.com>
2020-04-26 17:43:17 +0700

    cleaning

--- result_py_files/1 week/draw_ladder.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-value = 0 if len(sys.argv) != 2 else int(sys.argv[1])
-for i in range(value):
-    i += 1
-    print(" " * (value - i), "#" * i, sep="")
--- result_py_files/1 week/summing_numbers.py
+++
@@ -1,3 +0,0 @@
-import sys
-
-print(sum(map(int, [0] if len(sys.argv) != 2 else sys.argv[1])))
