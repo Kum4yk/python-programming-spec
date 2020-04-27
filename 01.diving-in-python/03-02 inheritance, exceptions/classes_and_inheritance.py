8847cfb8d7a0725e53656479f20534f244d8741a
Evgenyi Yakimchuk <Kum4yk@gmail.com>
2020-04-27 23:37:16 +0700

    done week 3

--- 01.diving-in-python/03-02 inheritance, exceptions/classes_and_inheritance.py
+++ 01.diving-in-python/03-02 inheritance, exceptions/classes_and_inheritance.py
@@ -1,14 +1,108 @@
+import csv
+
+
 class CarBase:
-    pass
+    def __init__(self, brand: str, photo_file_name: str, carrying: float):
+        self.brand = brand
+        self.photo_file_name = photo_file_name
+        self.carrying = carrying
+
+    def get_photo_file_ext(self):
+        return "." + self.photo_file_name.split(".")[-1]
 
 
 class Car(CarBase):
-    pass
+    def __init__(self, brand: str, photo_file_name: str, carrying: float, passenger_seats_count: int):
+        super().__init__(brand, photo_file_name, carrying)
+        self.passenger_seats_count = passenger_seats_count
 
 
 class Truck(CarBase):
-    pass
+    def __init__(self, brand: str, photo_file_name: str, carrying: float, body_whl: str):
+        super().__init__(brand, photo_file_name, carrying)
+        self.body_length, self.body_width, self.body_height = Truck.take_values_from_str(body_whl)
+
+    def get_params(self):
+        print(self.body_length, self.body_width, self.body_height)
+
+    @staticmethod
+    def take_values_from_str(body_whl):
+        answer = (0, 0, 0)
+        body_whl = body_whl.split("x")
+        if len(body_whl) == 3:
+            try:
+                answer = map(float, body_whl)
+            except ValueError:
+                answer = (0, 0, 0)
+        return answer
+
+    def get_body_volume(self):
+        return self.body_length * self.body_width * self.body_height
 
 
 class SpecMachine(CarBase):
-    pass
+    def __init__(self, brand, photo_file_name, carrying, extra):
+        super().__init__(brand, photo_file_name, carrying)
+        self.extra = extra
+
+
+def check_params(car_type: str, brand: str, passenger_seat_count: str, photo_file_name: str, body_whl, carrying: str,
+                 extra: str):
+    car_type = car_type.lower()
+    possible_cars = {"car", "truck", "spec_machine"}
+
+    if car_type not in possible_cars:
+        return False
+    if not brand:
+        return False
+
+    if photo_file_name.split(".")[-1] not in {"jpg", "jpeg", "png", "gif"}:
+        return False
+
+    if car_type is "Car" and (not passenger_seat_count or not passenger_seat_count.isdigit()):
+        return False
+    if car_type is "spec_machine" and not extra:
+        return False
+
+    try:
+        float(carrying)
+    except ValueError:
+        return False
+
+    return True
+
+
+def create_vehicle_from_params(params):
+    car_type, brand, passenger_seats_count, photo_file_name, body_whl, carrying, extra = params
+
+    carrying = float(carrying)
+    if car_type == "car":
+        passenger_seats_count = int(passenger_seats_count)
+        return Car(brand, photo_file_name, carrying, passenger_seats_count)
+
+    if car_type == "truck":
+        return Truck(brand, photo_file_name, carrying, body_whl)
+
+    return SpecMachine(brand, photo_file_name, carrying, extra)
+
+
+def get_car_list(csv_filename):
+    car_list = []
+    with open(csv_filename) as csv_fd:
+        reader = csv.reader(csv_fd, delimiter=';')
+        next(reader)  # пропускаем заголовок
+        for row in reader:
+            if len(row) != 7:
+                continue
+            if not check_params(*row):
+                continue
+            car_list.append(create_vehicle_from_params(row))
+    return car_list
+
+
+if __name__ == "__main__":
+    cars = get_car_list("coursera_week3_cars.csv")
+    for car in cars:
+        print(type(car))
+    print(cars[0].passenger_seats_count)
+    print(cars[1].get_body_volume())
