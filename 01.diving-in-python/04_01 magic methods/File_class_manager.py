f077154b6bcd318641f7167d1c57c62edbda324d
Evgenyi Yakimchuk <Kum4yk@gmail.com>
2020-04-28 20:38:35 +0700

    done week 4.1

---
+++ 01.diving-in-python/04_01 magic methods/File_class_manager.py
@@ -0,0 +1,40 @@
+import os
+import tempfile
+
+
+class File:
+    __counter = 0
+
+    def __init__(self, path_to_file):
+        self.path = path_to_file
+        File.__counter += 1
+        if not os.path.exists(path_to_file):
+            self.write("")
+
+    def read(self):
+        with open(self.path, "r") as file:
+            return file.read()
+
+    def write(self, text):
+        with open(self.path) as file:
+            file.write(text)
+
+    def __add__(self, other):
+        new_path = os.path.join(tempfile.gettempdir(), f'temp_file_{File.__counter}')
+        res = File(new_path)
+        res.write(self.read() + other.read())
+        return res
+
+    def __str__(self):
+        return self.path
+
+    def __iter__(self):
+        return open(self.path, "r")
+
+
+if __name__ == "__main__":
+    some_file = File("my_file")
+    some_text = "\n".join(map(str, range(10)))
+    some_file.write(some_text)
+    for some in some_file:
+        print(ascii(some))
