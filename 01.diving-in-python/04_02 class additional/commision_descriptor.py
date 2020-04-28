18eba458080342268305637368dd3f5cd259bf46
Evgenyi Yakimchuk <Kum4yk@gmail.com>
2020-04-28 22:09:04 +0700

    update week 4.2

---
+++ 01.diving-in-python/04_02 class additional/commision_descriptor.py
@@ -0,0 +1,30 @@
+class Value:
+    def __init__(self):
+        self.value = None
+
+    def __get__(self, obj, obj_type):
+        return self.value
+
+    def __set__(self, obj, value):
+        if not hasattr(obj, "commission"):
+            raise NotImplementedError(
+                f"class {type(obj).__name__} has no attribute \"commission\""
+            )
+        
+        result = value - value * obj.commission
+        result = int(result) if result - int(result) == 0 else result
+        self.value = result
+
+
+if __name__ == "__main__":
+    class Account:
+        amount = Value()
+
+        def __init__(self, commission):
+            self.some = commission
+
+
+    new_account = Account(0.1)
+    new_account.amount = 100
+
+    print(new_account.amount)
